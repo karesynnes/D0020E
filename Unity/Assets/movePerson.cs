2fe827475d51f553df8f4cbe33d3dd5c1044c86c
williamos98 <zwilliamosz@gmail.com>
2021-02-04 16:15:06 +0100

    boll
    rör sig wow

---
+++ Unity/Assets/movePerson.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movePerson : MonoBehaviour
+{
+    Vector3 yep = new Vector3(0, 0, 0.05f);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ta in widefind här och uppdatera positionen med dem.
+        transform.position += yep;
+    }
+}
