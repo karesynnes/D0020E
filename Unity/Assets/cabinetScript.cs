7aed0ef725d1ea2fd4ef15918a021cb6550752d7
williamos98 <zwilliamosz@gmail.com>
2021-03-10 15:24:21 +0100

    cabinetscript

---
+++ Unity/Assets/cabinetScript.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cabinetScript : SensorScript
+{
+    int angle = 75;
+    Transform left;
+    Transform right;
+    private int open;
+    GameObject a;
+    CommunicationScript script;
+
+
+    public cabinetScript()
+    {
+
+        this.open = 0;
+
+
+
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        a = GameObject.Find("Main Camera");
+
+        script = a.GetComponent<CommunicationScript>();
+        left = transform.Find("left");
+        right = transform.Find("right");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        //this.open = base.model.getInfo(base.sensorID)[0];
+        // print(base.model.getTest());
+        try
+        {
+            //print(script.getModel().getInfo(base.sensorID));
+            //print(base.sensorID);
+
+            this.open = script.getModel().getInfo(base.sensorID)[0];
+
+            //print(this.open);       
+
+
+            if (this.open == 1)
+            {
+                left.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.x, angle);
+                right.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.x, -angle);
+            }
+            else
+            {
+                left.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.x, 0);
+                right.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.x, 0);
+            }
+
+        }
+        catch (Exception e)
+        {
+            print(base.sensorID + " :" + e);
+
+
+        }
+
+    }
+}
